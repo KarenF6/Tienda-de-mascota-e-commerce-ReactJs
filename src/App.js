6a0a3bc3119243e5edf2068fd67a8147f9ba8f7f
karendayana19 <karendayanafuentes14@gmail.com>
2023-11-23 19:34:48 -0500

    carrito avances

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import Home from './componets/Home/Home';
-
+import CartContent from './componets/CartContent/CartContent';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 function App() {
-  return <Home/>;
-  
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/"  element={<Home/>}/>
+        <Route path='/cart' element={<CartContent />} />
+      </Routes>    
+    </BrowserRouter>
+  )  
 }
 
 export default App;
--- src/componets/Navbar/Navbar.js
+++ src/componets/Navbar/Navbar.js
@@ -1,10 +1,14 @@
 import "./Navbar.css";
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div className="nav-container">
         <nav className="navbar"> 
            <h1 className="navbar-logo">Mascoticas. </h1>
-           <h2 className="seeCarrito">🛒</h2>
+           <Link className="seeCarrito" to={"/cart"}>
+            🛒
+            </Link>
         </nav>
     </div>
   )
