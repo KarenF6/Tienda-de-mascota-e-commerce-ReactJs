d1c5f91b35c9b2030e5cf26891b1f6bdb7b6eaae
karendayana19 <karendayanafuentes14@gmail.com>
2023-11-23 19:04:57 -0500

    Banner y Navbar+ adelanto de carrito

---
+++ src/componets/Banner/Banner.js
@@ -0,0 +1,14 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="banner-container">
+            <h1>Tienda de mascotas</h1>
+            <p>El mejor lugar para tus mascoticas</p>
+        </div>
+    </div>
+
+  )
+}
+
+export default Banner
---
+++ src/componets/Navbar/Navbar.js
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+        <nav className="navbar"> 
+           <h1 className="navbar-logo">Mascoticas. </h1>
+           <h2 className="seeCarrito">🛒</h2>
+        </nav>
+    </div>
+  )
+
+}
+
+export default Navbar
