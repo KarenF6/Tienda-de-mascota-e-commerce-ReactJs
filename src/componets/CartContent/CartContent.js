321199f7684f4a903003bf706cc47aff026db63c
karendayana19 <karendayanafuentes14@gmail.com>
2023-11-24 19:03:32 -0500

    carrito, imagenes y producos

--- src/componets/CartContent/CartContent.js
+++ src/componets/CartContent/CartContent.js
@@ -1,7 +1,22 @@
+import { useContext } from "react";
+import { dataContext } from "../Context/DataContext";
+
+import CartElements from "./CartElements"
+import CartTotal from "./CartTotal";
+
 const CartContent = () => {
-  return (
-    <div>CartContent</div>
+  const {cart} = useContext(dataContext);
+
+
+  return cart.length > 0 ?(
+  <>
+  <CartElements />
+  <CartTotal/>
+  </>
+  ): (
+    <h2 className="cart.message.center"> Your cart is empty</h2>
   )
-}
+  
+};
 
 export default CartContent
--- src/componets/Products/Products.js
+++ src/componets/Products/Products.js
@@ -4,16 +4,21 @@ import { dataContext } from "../Context/DataContext";
 import "./Products.css";
 
 const Products = () => {
-   const{ data}= useContext(dataContext);
+   const{ data, cart, setCart}= useContext(dataContext);
+
+   const buyProducts = (product) => {
+    console.log(product);
+    setCart([...cart, product]);
+   };
    return data.map((product)=> {
     return(
-        <div className="card">
+        <div className="card" key={product.id}>
             <img src={product.img} alt="img-product-card"/>
             <h3>{product.name}</h3>
             <h4>{product.price}$</h4>
-            <button>buy</button>
+            <button onClick={()=> buyProducts (product)}>buy</button>
         </div>
-    )
+    );
    })
   
 };
